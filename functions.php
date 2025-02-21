f6c13a50832224eab7543f956d26dfdf10910293
Ngawang Tenzin <ngawang.tenzin1@my.jcu.edu.au>
2025-02-21 14:41:40 +1000

    Home Page v1.0

--- functions.php
+++ functions.php
@@ -6,5 +6,9 @@ function my_child_theme_enqueue_styles()
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
     //Load child theme styles
     wp_enqueue_style('child-style', get_stylesheet_uri(), array('parent-style'));
+
+    // Enqueue custom JavaScript file
+    wp_enqueue_script('main-js', get_stylesheet_directory_uri() . '/main.js', array(), false, true);
+
 }
 add_action('wp_enqueue_scripts', 'my_child_theme_enqueue_styles');
